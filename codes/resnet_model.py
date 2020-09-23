1b257a359f666bdd0c95b73f7c15583c2e132c02
saisriteja <48018142+saisriteja@users.noreply.github.com>
2020-09-23 19:46:10 +0530

    added fbeta as a accuracy metrics

--- codes/resnet_model.py
+++ codes/resnet_model.py
@@ -22,6 +22,7 @@ from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.resnet50 import preprocess_input, ResNet50
 import keras
 from keras import optimizers
+from keras import backend
 
 
 def resnet_model(size = (256,2048,1)):
@@ -166,3 +167,21 @@ def resnet_model(size = (256,2048,1)):
     # create model
     model = Model(inputs=input, outputs=out)
     return model
+
+
+# calculate fbeta score for multi-class/label classification
+def fbeta(y_true, y_pred, beta=2):
+	# clip predictions
+	y_pred = backend.clip(y_pred, 0, 1)
+	# calculate elements
+	tp = backend.sum(backend.round(backend.clip(y_true * y_pred, 0, 1)), axis=1)
+	fp = backend.sum(backend.round(backend.clip(y_pred - y_true, 0, 1)), axis=1)
+	fn = backend.sum(backend.round(backend.clip(y_true - y_pred, 0, 1)), axis=1)
+	# calculate precision
+	p = tp / (tp + fp + backend.epsilon())
+	# calculate recall
+	r = tp / (tp + fn + backend.epsilon())
+	# calculate fbeta, averaged across each class
+	bb = beta ** 2
+	fbeta_score = backend.mean((1 + bb) * (p * r) / (bb * p + r + backend.epsilon()))
+	return fbeta_score
